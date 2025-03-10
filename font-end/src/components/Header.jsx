3845d91b8f1f38504eb5951c33d6094f00c850fb
Him <vuqtpro1592002@gmail.com>
2025-03-10 18:01:17 +0700

    xong chuyen trang

--- font-end/src/components/Header.jsx
+++ font-end/src/components/Header.jsx
@@ -5,6 +5,48 @@ function Header() {
   const [company, setCompany] = useState(null);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  const scrollToPositionPart1 = () => {
+    const viewportHeight = window.innerHeight; 
+    window.scrollTo({
+      top: viewportHeight * 2,
+      behavior: "smooth",
+    });
+  };
+  const scrollToPositionHome = () => {
+    const viewportHeight = window.innerHeight; 
+    window.scrollTo({
+      top: viewportHeight * 0,
+      behavior: "smooth",
+    });
+  };
+  const scrollToPositionPart2 = () => {
+    const viewportHeight = window.innerHeight; 
+    window.scrollTo({
+      top: viewportHeight * 4,
+      behavior: "smooth",
+    });
+  };
+  const scrollToPositionPart3 = () => {
+    const viewportHeight = window.innerHeight; // Chiều cao viewport
+    window.scrollTo({
+      top: viewportHeight * 7.2,
+      behavior: "smooth",
+    });
+  };
+  const clickMobile1 = ()=>{
+    scrollToPositionPart1(),
+    setIsMenuOpen(!isMenuOpen);
+  }
+  const clickMobile2 = ()=>{
+    scrollToPositionPart2(),
+    setIsMenuOpen(!isMenuOpen);
+
+  }
+   const clickMobile3 = ()=>{
+    scrollToPositionPart3(),
+    setIsMenuOpen(!isMenuOpen);
+  }
+  
   useEffect(() => {
     const data = JSON.parse(localStorage.getItem("data"));
     if (data) {
@@ -13,8 +55,8 @@ function Header() {
   }, []);
 
   return (
-    <div className="w-screen flex h-15 bg-blue-700 p-5 text-white fixed justify-between items-center z-50 mb-auto md:h-15 lg:w-[80vw]">
-      <a className="bg-blue-700 cursor-pointer hover:bg-blue-600 flex items-center justify-center z-10 w-20 h-15">
+    <div className="w-screen flex h-15 bg-blue-700 p-5 text-white fixed justify-between items-center z-50 mb-auto md:h-15 lg:w-[80vw] select-none">
+      <a onClick={scrollToPositionHome} className="bg-blue-700 cursor-pointer hover:bg-blue-600 flex items-center justify-center z-10 w-20 h-15">
         <img src={company?.logo_img_url} className="invert mix-blend-screen w-15 h-15" alt="Company Logo" />
       </a>
 
@@ -24,9 +66,9 @@ function Header() {
           onClick={() => setIsMenuOpen(!isMenuOpen)} 
         />
         <ul className="hidden md:flex items-center gap-12">
-          <li className="p-3 hover:bg-blue-500 cursor-pointer transition-all">PHẦN I</li>
-          <li className="p-3 hover:bg-blue-500 cursor-pointer transition-all">PHẦN II</li>
-          <li className="p-3 hover:bg-blue-500 cursor-pointer transition-all">PHẦN III</li>
+          <li className="p-3 hover:bg-blue-500 cursor-pointer transition-all" onClick={scrollToPositionPart1 }>PHẦN I</li>
+          <li className="p-3 hover:bg-blue-500 cursor-pointer transition-all" onClick={scrollToPositionPart2}>PHẦN II</li>
+          <li className="p-3 hover:bg-blue-500 cursor-pointer transition-all" onClick={scrollToPositionPart3}>PHẦN III</li>
         </ul>
       </div>
 
@@ -36,9 +78,9 @@ function Header() {
         }`}
         style={{ transition: "transform 0.3s ease, opacity 0.3s ease" }}
       >
-        <li className="p-3 hover:bg-blue-500 cursor-pointer transition-all">PHẦN I</li>
-        <li className="p-3 hover:bg-blue-500 cursor-pointer transition-all">PHẦN II</li>
-        <li className="p-3 hover:bg-blue-500 cursor-pointer transition-all">PHẦN III</li>
+        <li className="p-3 hover:bg-blue-500 cursor-pointer transition-all" onClick={clickMobile1}>PHẦN I</li>
+        <li className="p-3 hover:bg-blue-500 cursor-pointer transition-all" onClick={clickMobile2}>PHẦN II</li>
+        <li className="p-3 hover:bg-blue-500 cursor-pointer transition-all" onClick={clickMobile3}>PHẦN III</li>
       </div>
 
       <div className="p-4">ToPDF</div>
